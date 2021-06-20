88e84ba1d4a28d24702ebb8fcbd649918d58e6f0
makbarish <80001675+makbarish@users.noreply.github.com>
2021-06-20 10:14:50 -0500

    modified the query

--- EmployeeSQL/QuickDBD-Employee_db.sql
+++ EmployeeSQL/QuickDBD-Employee_db.sql
@@ -85,13 +85,15 @@ REFERENCES "employee" ("emp_title_id");
 
 --1. List the following details of each employee: employee number, last name, first name, sex, and salary.
 
-select employee.emp_no, last_name, first_name, sex, salary from employee
+select employee.emp_no, last_name, first_name, sex, salary 
+from employee
 join salaries on employee.emp_no = salaries.emp_no
 
 
 --2. List first name, last name, and hire date for employees who were hired in 1986.
 
-select first_name, last_name, hire_date from employee
+select first_name, last_name, hire_date 
+from employee
 --WHERE hire_date >= '1986-01-01' and hire_date <= '1986-12-31';
 WHERE hire_date > '1986-01-01' and hire_date < '1987-01-01';
 
@@ -118,16 +120,26 @@ WHERE first_name = 'Hercules' and last_name like 'B%';
 
 --6. List all employees in the Sales department, including their employee number, last name, first name, and department name.
 
-
+select dept_emp.emp_no, employee.last_name, employee.first_name, dept_name
+from dept_emp
+join departments on dept_emp.dept_no = departments.dept_no
+join employee on employee.emp_no = dept_emp.emp_no
+where dept_name = 'Sales'
 
 --7. List all employees in the Sales and Development departments, including their employee number, last name, first name, and department name.
 
-
+select dept_emp.emp_no, employee.last_name, employee.first_name, dept_name
+from dept_emp
+join departments on dept_emp.dept_no = departments.dept_no
+join employee on employee.emp_no = dept_emp.emp_no
+where dept_name = 'Sales' or dept_name = 'Development'
 
 --8. In descending order, list the frequency count of employee last names, i.e., how many employees share each last name.
 
-
-
+select last_name, count (last_name) as "Frequency"
+from employee
+group by last_name
+order by "Frequency" desc
 
 
 
